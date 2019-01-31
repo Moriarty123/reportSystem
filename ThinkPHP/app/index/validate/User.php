5eff85e914a4045e7c5386eb4a0d97f80c866242
Moriarty123 <1561357687@qq.com>
2019-01-31 18:31:50 +0800

    2019-1-31 index 登录系统
    Signed-off-by: Moriarty123 <1561357687@qq.com>

---
+++ ThinkPHP/app/index/validate/User.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace app\index\validate;
+
+use think\Validate;
+
+class User extends Validate
+{
+	//验证规则	
+    protected $rule = [
+        'account|账号'  =>  'require|length:7,12',
+        'password|密码' =>  'require|min:6|max:12',
+    ];
+
+    //验证信息
+    protected $message = [
+    	'account.require' 		=> '账号不能为空',
+    	'account.length'		=> '请输入7位或12位账号',
+    	'password.require' 		=> '密码不能为空',
+    	'password.min' 			=> '密码最少为6位',
+    	'password.max' 			=> '密码最多为12位'
+    ];
+
+}
