375606da3a1127dd422da57d7c5d8aefa734c109
Moriarty123 <1561357687@qq.com>
2019-01-31 18:31:50 +0800

    2019-1-31 index 登录系统
    Signed-off-by: Moriarty123 <1561357687@qq.com>

--- ThinkPHP/app/index/controller/Index.php
+++ ThinkPHP/app/index/controller/Index.php
@@ -5,8 +5,15 @@ use think\Controller;
 
 class Index extends Controller
 {
+	// 首页
     public function index()
     {
         return $this->fetch('index');
+    }
+
+    // 登录页面
+    public function login() 
+    {
+    	return $this->fetch('login');
     }
 }
