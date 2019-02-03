096d76d9a51cc739f382838d6167d115792b55cb
Moriarty123 <1561357687@qq.com>
2019-02-03 20:03:23 +0800

    2019-2-3 teacher 实验指导列表
    Signed-off-by: Moriarty123 <1561357687@qq.com>

--- ThinkPHP/app/teacher/model/Guide.php
+++ ThinkPHP/app/teacher/model/Guide.php
@@ -3,7 +3,23 @@ namespace app\teacher\model;
 
 use think\Model;
 
+use app\teacher\model\Task as taskModel;
+
 class Guide extends Model
 {
-    
+    //
+    public function getTaskNoAttr($value)
+    {
+    	if($value == null) {
+    		return '';
+    	}
+    	else {
+    		$taskModel = new taskModel();
+
+    		$where = "taskNo = '$value'";
+    		$task = $taskModel->where($where)->find();
+
+    		return $task['taskName'];
+    	}
+    }
 }
