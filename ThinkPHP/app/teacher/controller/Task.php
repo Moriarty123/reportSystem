a709a781b54c27f88057a0e4c009fea7350ac29e
Moriarty123 <1561357687@qq.com>
2019-02-06 11:28:00 +0800

    2019-2-6 teacher 实验课程查看实验任务
    Signed-off-by: Moriarty123 <1561357687@qq.com>

--- ThinkPHP/app/teacher/controller/Task.php
+++ ThinkPHP/app/teacher/controller/Task.php
@@ -47,4 +47,40 @@ class Task extends Common
 		return $this->fetch('taskList');
     }
 
+    //查看实验任务
+    public function courseTask()
+    {
+        //0.测试
+        // dump($_GET);
+
+        Log::record('显示实验任务列表','notice');
+
+        //1.获取账号
+        $account = session('account');
+        $courseNo = input('get.courseNo');
+
+        //2.获取该账号教师的实验任务
+        $teacherModel = new teacherModel();
+
+        $where = "a.teacherNo = '$account' and b.courseNo = '$courseNo'";
+
+        $taskList = $teacherModel   ->task()
+                                    ->where($where)
+                                    ->alias('a')
+                                    ->join('course b', 'a.courseNo = b.courseNo')
+                                    ->paginate(15);
+
+        $taskNumber = $teacherModel ->task()
+                                    ->where($where)
+                                    ->alias('a')
+                                    ->join('course b', 'a.courseNo = b.courseNo')
+                                    ->count();
+
+        //3.页面渲染
+        $this->assign('taskList', $taskList);
+        $this->assign('taskNumber', $taskNumber);
+
+        return $this->fetch('taskList');
+    }
+
 }
