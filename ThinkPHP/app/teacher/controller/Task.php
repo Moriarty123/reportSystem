e3aae8448aad4793fa868e7c3e07b9969f3c7dc7
Moriarty123 <1561357687@qq.com>
2019-02-06 11:54:21 +0800

    2019-2-6 teacher 模糊搜索实验任务
    Signed-off-by: Moriarty123 <1561357687@qq.com>

--- ThinkPHP/app/teacher/controller/Task.php
+++ ThinkPHP/app/teacher/controller/Task.php
@@ -83,4 +83,56 @@ class Task extends Common
         return $this->fetch('taskList');
     }
 
+    //模糊查找实验任务
+    public function taskSearch()
+    {
+        //0.测试
+        // dump($_POST); 
+        Log::record('模糊查找实验任务','notice');
+
+        //1.获取数据
+        $search = input('post.search');
+        $account = session('account');
+
+        //2.获取该账号教师的实验实验指导
+        //2.1构造搜索条件
+        if(!empty($search)) {
+            session('taskSearch', $search);
+            $where['taskName'] = array('like','%'.$search.'%');//封装模糊查询 赋值到数组  
+        }
+        else 
+        {
+            $search = session('taskSearch');
+            $where['taskName'] = array('like','%'.$search.'%');    
+        }
+
+        //2.2获取符合条件的实验指导
+        $teacherModel = new teacherModel();
+
+        $teacherNoWhere = "a.teacherNo = '$account'";
+
+        $guideList = $teacherModel ->task()
+                                    ->where($where)
+                                    ->where($teacherNoWhere)
+                                    ->alias('a')
+                                    ->join('course b', 'a.courseNo = b.courseNo')
+                                    ->paginate(15);
+
+        $guideNumber = $teacherModel    ->task()
+                                        ->where($where)
+                                        ->where($teacherNoWhere)
+                                        ->alias('a')
+                                        ->join('course b', 'a.courseNo = b.courseNo')
+                                        ->count();
+
+        //3.页面渲染
+        $this->assign('taskList', $guideList);
+        $this->assign('taskNumber', $guideNumber);
+
+
+        return $this->fetch('taskList');
+
+    }
+
+
 }
